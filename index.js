333a0aba585d5a7d6c8476f58bb2c6435b487c19
Ali <ali-a-ahmed1097@hotmail.com>
2023-01-26 19:03:47 -0500

    Can now build a balance BST. Added prettyPrint function to see the created BST.

--- index.js
+++ index.js
@@ -14,10 +14,33 @@ class Node {
 
 class Tree {
     constructor(arr) {
-        this._treeArr = arr;
+        this._root = this.buildTree(arr);
     }
 
-    buildTree() {
+    getRoot() { return this._root; }
+
+    buildTree(arr) {
+        let treeArr = [...new Set(arr)];
+        treeArr.sort((a, b) => a - b);
+
+        if (treeArr.length === 0) return null;
         
+        let mid = Math.floor((treeArr.length - 1) / 2);
+        let root = new Node(treeArr[mid]);
+        
+        root.setLeftChild(this.buildTree(treeArr.slice(0, mid)));
+        root.setRightChild(this.buildTree(treeArr.slice(mid+1)));
+        
+        return root;
     }
 }
+
+const prettyPrint = (node, prefix = '', isLeft = true) => {
+    if (node.getRightChild() !== null) {
+      prettyPrint(node.getRightChild(), `${prefix}${isLeft ? '│   ' : '    '}`, false);
+    }
+    console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.getValue()}`);
+    if (node.getLeftChild() !== null) {
+      prettyPrint(node.getLeftChild(), `${prefix}${isLeft ? '    ' : '│   '}`, true);
+    }
+  }
